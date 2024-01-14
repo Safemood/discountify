0f83482e3f87f4ec1e6b67f337808558d56ee9a4
Khalil Bouzidi <kbouziditn@gmail.com>
2024-01-14 20:19:08 +0100

    add Facade for Discountify class

--- src/Facades/Discountify.php
+++ src/Facades/Discountify.php
@@ -3,12 +3,36 @@
 namespace Safemood\Discountify\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Safemood\Discountify\ConditionManager;
 
 /**
+ * Class Discountify
+ *
+ * @method static \Safemood\Discountify\Discountify discount(float $globalDiscount)
+ * @method static array getConditions()
+ * @method static ConditionManager getConditionManager()
+ * @method static int getGlobalDiscount()
+ * @method static float getGlobalTaxRate()
+ * @method static array getItems()
+ * @method static float subtotal()
+ * @method static float tax()
+ * @method static float total()
+ * @method static float totalWithDiscount(?float $globalDiscount = null)
+ * @method static float tax(?float $globalTaxRate = null)
+ * @method static \Safemood\Discountify\Discountify setConditionManager(ConditionManager $conditionManager)
+ * @method static \Safemood\Discountify\Discountify setGlobalDiscount(int $globalDiscount)
+ * @method static \Safemood\Discountify\Discountify setGlobalTaxRate(float $globalTaxRate)
+ * @method static \Safemood\Discountify\Discountify setItems(array $items)
+ *
  * @see \Safemood\Discountify\Discountify
  */
 class Discountify extends Facade
 {
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
     protected static function getFacadeAccessor()
     {
         return \Safemood\Discountify\Discountify::class;

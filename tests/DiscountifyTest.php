6fd1331b2f228ba69d103f5cf9cf23ed6c407150
Khalil Bouzidi <kbouziditn@gmail.com>
2024-02-11 16:25:30 +0100

    refactor tests

--- tests/DiscountifyTest.php
+++ tests/DiscountifyTest.php
@@ -7,7 +7,6 @@ use Safemood\Discountify\ConditionManager;
 use Safemood\Discountify\CouponManager;
 use Safemood\Discountify\Discountify;
 use Safemood\Discountify\Events\DiscountAppliedEvent;
-use Safemood\Discountify\Exceptions\DuplicateSlugException;
 use Safemood\Discountify\Facades\Condition;
 use Safemood\Discountify\Facades\Coupon;
 use Safemood\Discountify\Facades\Discountify as DiscountifyFacade;
@@ -103,29 +102,6 @@ it('can get subtotal', function () {
     expect($subtotal)->toBe(floatval(200));
 });
 
-it('can add conditions using ConditionManager', function () {
-    $conditionManager = new ConditionManager();
-    $conditionManager
-        ->define('more_than_2_products_10', fn (array $items) => count($items) > 2, 10)
-        ->add([
-            [
-                'slug' => 'more_than_3_products_15',
-                'condition' => fn ($items) => count($items) > 3,
-                'discount' => 15,
-            ],
-            [
-                'slug' => 'special_type_product_10',
-                'condition' => fn ($items) => in_array('special', array_column($items, 'type')),
-                'discount' => 10,
-            ],
-        ])
-        ->defineIf('client_has_renewal_10', true, 10);
-
-    $conditions = $conditionManager->getConditions();
-
-    expect($conditions)->toHaveCount(4);
-});
-
 it('can use Condition facade', function () {
 
     Condition::define('more_than_2_products_10', fn (array $items) => count($items) > 2, 10)
@@ -212,40 +188,6 @@ it('calculates total with discount using dynamically set custom price field name
     expect($totalWithDiscount)->toBe(floatval(30));
 });
 
-it('throws exception when slug is not provided in define method', function () {
-    expect(fn () => Condition::define('', fn ($items) => count($items) > 5, 10))
-        ->toThrow(Exception::class, 'Slug must be provided.');
-});
-
-it('throws exception when slug is not provided in defineIf method', function () {
-    expect(fn () => Condition::defineIf('', true, 10))
-        ->toThrow(Exception::class, 'Slug must be provided.');
-});
-
-it('can auto register condition classes', function () {
-
-    Condition::discover(
-        'Workbench\\App\\Conditions',
-        workbench_path('app/Conditions')
-    );
-
-    $definedConditions = Condition::getConditions();
-
-    expect($definedConditions)
-        ->toBeArray()
-        ->toHaveCount(2)
-        ->each(function ($item) {
-            $item->toHaveKeys([
-                'slug',
-                'condition',
-                'discount',
-            ]);
-            $item->slug->toBeString();
-            $item->condition->toBeInstanceOf(Closure::class);
-            $item->discount->toBeFloat();
-        });
-});
-
 it('can work correctly with class-based conditions', function () {
 
     $items = [
@@ -270,52 +212,6 @@ it('can work correctly with class-based conditions', function () {
     expect($totalWithDiscount)->toBe(floatval(100));
     expect($totalWithTaxes)->toBe(floatval(238));
     expect($taxRate)->toBe(floatval(38));
-});
-
-test('it skips conditions marked with "skip"', function () {
-
-    $conditions = [
-        ['slug' => 'condition_1', 'condition' => fn () => true, 'discount' => 10],
-        ['slug' => 'condition_2', 'condition' => fn () => false, 'discount' => 20, 'skip' => true],
-        ['slug' => 'condition_3', 'condition' => fn () => true, 'discount' => 30, 'skip' => false],
-    ];
-
-    Condition::add($conditions);
-
-    $finalConditions = Condition::getConditions();
-
-    $this->assertCount(2, $finalConditions);
-});
-
-test('it throws an exception for conditions without "slug"', function () {
-    $this->expectException(InvalidArgumentException::class);
-
-    $conditions = [
-        ['condition' => fn () => true, 'discount' => 10],
-    ];
-
-    Condition::add($conditions);
-});
-
-test('it skips class-based conditions marked with "skip"', function () {
-
-    Condition::discover(
-        'Workbench\\App\\Conditions',
-        workbench_path('app/Conditions')
-    );
-
-    $conditions = Condition::getConditions();
-
-    expect($conditions)->toHaveCount(2);
-});
-
-it('ensures condition slugs are unique', function () {
-
-    Condition::define('unique_condition', fn () => true, 10);
-
-    $this->expectException(DuplicateSlugException::class);
-
-    Condition::define('unique_condition', fn () => true, 15);
 });
 
 it('can create a new condition class', function () {
